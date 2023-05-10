ac72b730635f7131aa9d926ca5dc8b488ef3d8f4
Andrew Elmendorf <aelmendorf234@gmail.com>
2023-05-10 13:50:54 -0400

    added browser refresh to bulkgas

---
+++ MonitoringSystem.Shared/Data/AmmoniaCalibrationData.cs
@@ -0,0 +1,11 @@
+﻿namespace MonitoringSystem.Shared.Data; 
+
+public class AmmoniaCalibrationData {
+    public int CalZero { get; set; }
+    public int CalNonZero { get; set; }
+    public int ActualZero { get; set; }
+    public int ActualNonZero { get; set; }
+    public int TotalWeight { get; set; }
+    public int GasWeight { get; set; }
+    public ushort Tank { get; set; }
+}
