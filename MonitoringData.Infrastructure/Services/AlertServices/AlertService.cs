207f8b61c92ab2e3927e46d48c75e7175c129085
Andrew Elmendorf <aelmendorf234@gmail.com>
2023-06-07 09:17:10 -0400

    github actions infrastructure 7

--- MonitoringData.Infrastructure/Services/AlertServices/AlertService.cs
+++ MonitoringData.Infrastructure/Services/AlertServices/AlertService.cs
@@ -15,7 +15,7 @@ namespace MonitoringData.Infrastructure.Services.AlertServices {
         Task Load();
         Task Reload();
     }
-    //Testing github actions infrastructure 6
+    //Testing github actions infrastructure 7
     public class AlertService : IAlertService {
         private readonly IAlertRepo _alertRepo;
         private readonly ILogger<AlertService> _logger;
